4d393871ed2d8d2e806e64dbdd37cc0382813d2e
miladevweb <miladevweb@outlook.com>
2024-10-04 10:09:43 -0500

    delete image in work/all route

--- src/app/work/all/page.tsx
+++ src/app/work/all/page.tsx
@@ -5,18 +5,19 @@ import { ProjectVisit } from '@/shared/components'
 export default function Page() {
   return (
     <>
-      <div className="col-breakout leading-none flex justify-center lg:justify-start items-center h-[70px] sm:h-[100px]  lg:h-[130px]">
+      <div className="col-breakout leading-none flex justify-center lg:justify-start items-center h-[70px] sm:h-[100px] lg:h-[130px] mt-2 sm:mt-5">
         <h2 className="font-semibold text-[40px] sm:text-6xl lg:text-[100px]">All Projects</h2>
         <sup className="text-[10px] sm:text-sm lg:text-base -translate-y-1 lg:-translate-y-4">({works.length})</sup>
       </div>
 
       <div className="col-breakout grid lg:grid-cols-[85%] 2xl:grid-cols-[70%]">
         <p className="opacity-70 sm:text-2xl xl:text-3xl">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis nisi tenetur esse deserunt facere voluptatem vero dicta ad eos recusandae!
+          Full stack web development projects combining dynamic front-end interfaces, robust back-end systems, and optimized databases. Focused on delivering seamless user experiences, clean code, and
+          responsive designs for all devices.
         </p>
       </div>
 
-      <div className="col-breakout justify-self-center my-5 lg:my-7 w-full md:w-[35vw] 2xl:w-[30vw]k">
+      {/* <div className="col-breakout justify-self-center my-5 lg:my-7 w-full md:w-[35vw] 2xl:w-[30vw]">
         <picture className="aspect-[4/3] relative">
           <Image
             fill
@@ -26,7 +27,7 @@ export default function Page() {
             sizes="(max-width: 640px) 50vw, 30vw"
           />
         </picture>
-      </div>
+      </div> */}
 
       <ProjectVisit />
     </>
